ee64f134d63c50fc0835ae07c1a556423dd3fb90
Илья Александрович Чмак <chmak.i@365.polessu.by>
2021-06-21 17:57:07 +0300

    Исправления замечаний -построчный анализ -учет возможных ошибок -внешний вид -оптимизация процесса (насколько мне видно)

--- Chmak_dNet/Program.cs
+++ Chmak_dNet/Program.cs
@@ -7,18 +7,46 @@ namespace Chmak_dNet
 {
     class Program
     {
-        static void Serialize(TextAnalysis analysis ,string filepath)
+        static void Serialize(TextAnalysis analysis ,string serializepath)
         {
             var json = JsonConvert.SerializeObject(analysis, Formatting.Indented);
             Console.WriteLine(json);
-            File.WriteAllText(filepath, json);
+            File.WriteAllText(serializepath, json);
         }
 
         static void Main(string[] args)
         {
-            // Создаем объект указывая ПУТЬ к файлу (в моём случае файл в корневой папке проекта)
-            TextAnalysis analysis = new TextAnalysis("text.txt");
-            Serialize(analysis, "serialized.json");
+            //TextAnalysis analysis = new TextAnalysis();
+            while (true)
+            {
+                Console.Write("Введите название файла для аназиза (разрешение .txt добавиться автоматически) : ");
+                string textpath = Console.ReadLine() + ".txt";
+                try
+                {
+                    // Создаем объект указывая ПУТЬ к файлу (в моём случае файл в корневой папке проекта)
+                    TextAnalysis analysis = new TextAnalysis(textpath);
+                    Console.Write("--файл проанализирован--" +
+                        "\nЕсли вы хотите сохранить результат в формате JSON," +
+                        " введите название (путь) файла для сохраниения" +
+                        "(разрешение .txt добавиться автоматически) : ");
+                    string serializepath = Console.ReadLine() + ".json";
+                    try
+                    {
+                        Serialize(analysis, serializepath);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("что-то пошло не так (возможно имя файла указано не верно)");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("что-то пошло не так (имя файла указано не верно)");
+                }
+
+
+            }
+
         }
     }
 }
